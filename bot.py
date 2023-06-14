de154b7f9f439fdf4edbc83516c377531690a6a6
EVGENIY ALEYNIKOV <on1xas509@gmail.com>
2023-06-15 00:26:22 +0300

    Create db connection, create table

--- bot.py
+++ bot.py
@@ -9,12 +9,15 @@ from keyboards.menu import set_main_menu
 from handlers.user_handlers import user_router
 from handlers.admin_handlers import admin_router
 from middleware.out_middleware import ConfigMiddleware
+from database.database import start_sqlite
 
 config: Config = load_config(".env")
 
 bot: Bot = Bot(config.tg_bot.token)
 dp: Dispatcher = Dispatcher()
 
+async def start_on():
+    await start_sqlite()
 # Регистрируем роутеры
 dp.include_router(admin_router)
 admin_router.message.outer_middleware(ConfigMiddleware(config))
@@ -24,4 +27,5 @@ dp.include_router(user_router)
 dp.startup.register(set_main_menu)
 
 if __name__ == "__main__":
+    asyncio.run(start_on())
     asyncio.run(dp.run_polling(bot))
--- keyboards/menu.py
+++ keyboards/menu.py
@@ -6,5 +6,5 @@ from lexicon.lexicon import LEXICON_COMMANDS_RU
 
 async def set_main_menu(bot: Bot):
     main_menu = [BotCommand(command=key, description=value) for key, value in LEXICON_COMMANDS_RU.items()]
-    print("Кнопка Меню успешно инициализирована")
+    print("Menu button init")
     await bot.set_my_commands(main_menu)
