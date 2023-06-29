106eef06c01214ef801cbb4dd494534d3e5d5cc0
e.aleynikov <on1xas509@gmail.com>
2023-06-29 11:17:01 +0300

    no

--- bot.py
+++ bot.py
@@ -10,9 +10,6 @@ from keyboards.menu import set_main_menu
 from handlers.user_handlers import user_router
 from handlers.admin_handlers import admin_router
 from middleware.out_middleware import ConfigMiddleware
-from database.database import start_sqlite
-from aiogram_dialog import setup_dialogs
-
 
 
 config: Config = load_config(".env")
@@ -23,17 +20,13 @@ bot: Bot = Bot(config.tg_bot.token)
 dp: Dispatcher = Dispatcher(storage=storage)
 
 
-async def start_on():
-    await start_sqlite()
 # Регистрируем роутеры
 dp.include_router(admin_router)
 admin_router.message.outer_middleware(ConfigMiddleware(config))
 dp.include_router(user_router)
-setup_dialogs(dp)
 
 # Регистрируем кнопку "Меню"
 dp.startup.register(set_main_menu)
 
 if __name__ == "__main__":
-    asyncio.run(start_on())
     asyncio.run(dp.run_polling(bot))
